5a43f4d5ff05749779c9097c12eb28d4655a0f20
Madeline <moon.madeline@gmail.com>
2023-07-14 10:41:21 -0500

    Refactor steps 1-4

--- scripts.js
+++ scripts.js
@@ -1,20 +1,16 @@
+let buttons = document.querySelectorAll('button');
+buttons.forEach(button => button.addEventListener('click', (target) => {
+    clickHandler(target.target.id);
+}))
 
-
-
-
-window.addEventListener('load', () => {
-    const takeOffButton = document.querySelector('#takeoff');
+function clickHandler (clickedButton) {
+    console.log(clickedButton);
     const flightStatus = document.querySelector('#flightStatus');
     const shuttleBackground = document.querySelector('#shuttleBackground');
     const spaceShuttleHeight = document.querySelector('#spaceShuttleHeight');
     let shuttleHeight = Number(spaceShuttleHeight.innerText);
-    const rocket = document.querySelector('#rocket');
-    const leftButton = document.querySelector('#left');
-    const rightButton = document.querySelector('#right');
-    let left = 0;
-    let right = 0;
 
-    takeOffButton.addEventListener('click', () => {
+    if (clickedButton == "takeoff") {
         const confirmation = window.confirm('Confirm that the shuttle is ready for takeoff.');
         if (confirmation) {
             flightStatus.textContent = 'Shuttle in flight.';
@@ -23,36 +19,37 @@ window.addEventListener('load', () => {
             spaceShuttleHeight.textContent = (shuttleHeight * 1000);
             rocket.style.bottom = `${shuttleHeight}px`;
         }
-    })
-
-    const landButton = document.querySelector('#landing');
-    landing.addEventListener('click', () => {
+    }
+    if (clickedButton == "landing") {
         window.alert('The shuttle is landing. Landing gear engaged.');
         flightStatus.textContent = 'The shuttle has landed.';
         shuttleBackground.style.backgroundColor = 'green';
         shuttleHeight = 0;
         spaceShuttleHeight.textContent = shuttleHeight;
         rocket.style.bottom = `${shuttleHeight}px`;
-    })
-
-    const abortButton = document.querySelector('#missionAbort');
-    abortButton.addEventListener('click', () => {
+    }
+    if (clickedButton == "missionAbort"){
         const abortFlight = window.confirm('Confirm that you want to abort the mission.') 
         if (abortFlight) {
             flightStatus.textContent = 'Mission aborted';
             shuttleBackground.style.backgroundColor = 'green';
             shuttleHeight = 0;
-            spaceShuttleHeight.textContent = shuttleHeight;
-        }
-    })
-
-    leftButton.addEventListener('click', () =>{
-        left += 10;
-        right -= 10;
-        rocket.style.left = `${left}px`;
-    })
+            spaceShuttleHeight.textContent = shuttleHeight;}
+    }
+}
+    
 
 
+    // const leftButton = document.querySelector('#left');
+    // const rightButton = document.querySelector('#right');
+    // let left = 0;
+    // let right = 0;
+
+
+    // leftButton.addEventListener('click', () =>{
+    //     left += 10;
+    //     right -= 10;
+    //     rocket.style.left = `${left}px`;
+    // })
 
     // rocket.style.bottom += 
-})
